92a3d8c626254b9b0dc6b2f192cb5f6006b9e4c2
lamecksilva <lameck2@hotmail.com>
2019-03-03 18:00:36 -0300

    Creating app, package.json and express Hello World app :yum:

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
